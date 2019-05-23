e7449beb31d6fdcf60249d5f11df2633f1ee155d
Fika Ridaul Maulayya <ridaulmaulayya@gmail.com>
2019-05-23 13:39:28 +0700

    Create mac-address.php

---
+++ mac-address.php
@@ -0,0 +1,17 @@
+<?php
+
+function mac(){
+	
+	ob_start();
+	system('ipconfig/all');
+	$mycom = ob_get_contents();
+	ob_clean();
+
+	$findme = "Physical";
+	$pmac = strpos($mycom, $findme);
+	$mac= substr($mycom, ($pmac+36),17);
+	echo "MAC ADDRESS = {$mac}";
+
+}
+
+mac();
